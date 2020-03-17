69748508b9d6e77e8ea6bdfc0ade193dadbb7685
DmitriVlassenko <dmitrivlassenko@gmail.com>
2020-03-17 11:33:41 +0200

    ft_putnbr

---
+++ ft_putnbr/ft_putnbr.c
@@ -0,0 +1,21 @@
+void	ft_putchar(char c);
+
+void	ft_putnbr(int nb)
+{
+	
+	long long int	nbr;
+
+	nbr = nb;
+	if (nbr < 0)
+	{
+		ft_putchar('-');
+		nbr = -nbr;
+	}
+	if (nbr > 9)
+	{
+		ft_putnbr(nbr / 10);
+		ft_putnbr(nbr % 10);
+	}
+	else
+		ft_putchar(nbr + '0');
+}
