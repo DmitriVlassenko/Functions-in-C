d03cef083babe762dca017b87067b53f7532973a
DmitriVlassenko <dmitrivlassenko@gmail.com>
2020-03-18 19:50:00 +0200

    ft_strcapitalize

---
+++ ft_strcapitalize.c/ft_strcapitalize.c
@@ -0,0 +1,28 @@
+char	*ft_lowcase(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		if (str[i] >= 'A' && str[i] <= 'Z')
+			str[i] = str[i] + 32;
+		i++;
+	}
+	return (str);
+}
+
+char	*ft_strcapitalize(char *str)
+{
+	int	i;
+
+	i = 0;
+	ft_lowcase(str);
+	while (str[i] != '\0')
+	{
+		if ((i == 0 || str[i - 1] < '0') && (str[i] >= 'a' && str[i] <= 'z'))
+			str[i] = str[i] - 32;
+		i++;
+	}
+	return (str);
+}
